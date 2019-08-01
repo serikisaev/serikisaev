4c759ac6a6a5246213f3dab5320c0d2ad09ef39b
serikisaev <42213906+serikisaev@users.noreply.github.com>
2019-08-01 20:23:14 +0500

    Update q_sort1.py

--- q_sort1.py
+++ q_sort1.py
@@ -1,21 +1,27 @@
 import random
+from random import randint
 
 def q_sort(mass):
-    first_mass = []
-    second_mass = []
-    third_mass = []
-    a = random.choice(mass)
+    if len(mass) <= 1:
+       return mass
+    else:
+        first_mass = []
+        second_mass = []
+        third_mass = []
+        a = random.choice(mass)
+        for i in mass:
+            if i < a:
+                first_mass.append(i)
+            elif i > a:
+                second_mass.append(i)
+            else:
+                third_mass.append(i)
+        return q_sort(first_mass) + third_mass + q_sort(second_mass)
     
-    for i in mass:
-        if i < a:
-            first_mass.append(i)
-        elif i > a:
-            second_mass.append(i)
-        else:
-            third_mass.append(i)
-    return q_sort(first_mass) + third_mass + q_sort(second_mass)
-    
-pikabu = [5,9,7,6,2,4,56,6,8,5]
+n = 15
+pikabu = []
+for i in range(n):
+    pikabu.append(randint(1, 99))
 print(pikabu)
 print("--------------------------------------")
-q_sort(pikabu)
+print(q_sort(pikabu))
